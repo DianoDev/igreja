9d366f00b85c1e5360dc8fa86bf79c8be3c04941
dianodev <adrianomellocolombo@gmail.com>
2025-11-04 14:00:09 -0400

    build

---
+++ bootstrap/ssr/assets/AvisosForm-Bun0nOuI.js
@@ -0,0 +1,152 @@
+import { ref, inject, onMounted, mergeProps, unref, useSSRContext } from "vue";
+import { ssrRenderAttrs, ssrRenderComponent, ssrInterpolate, ssrIncludeBooleanAttr, ssrLooseContain } from "vue/server-renderer";
+import _sfc_main$1 from "./InputLabel-CDsQQ3SF.js";
+import _sfc_main$3 from "./InputError-DKvItylr.js";
+import _sfc_main$2 from "./TextInput-BEEeI7hx.js";
+import { useToast } from "vue-toastification";
+import VueDatePicker from "@vuepic/vue-datepicker";
+/* empty css              */
+const _sfc_main = {
+  __name: "AvisosForm",
+  __ssrInlineRender: true,
+  props: {
+    data: {
+      type: Object,
+      default: null,
+      required: false
+    }
+  },
+  emits: ["close"],
+  setup(__props, { emit: __emit }) {
+    const props = __props;
+    const acao = ref("/admin/avisos/create");
+    inject("events");
+    const toast = useToast();
+    const errors = ref({});
+    const processing = ref(false);
+    const ready = ref(false);
+    const readOnly = ref(false);
+    const form = ref({
+      nome: "",
+      descricao: "",
+      data_expiração: null,
+      ativo: false
+    });
+    const loadData = async () => {
+      try {
+        const response = await axios.get(`/admin/avisos/${props.data.id}`);
+        Object.keys(form.value).forEach((key) => {
+          if (response.data[key] !== void 0) {
+            if (key === "ativo") {
+              form.value[key] = response.data[key] === 1 || response.data[key] === "1" || response.data[key] === true;
+            } else if (key === "data_expiração" && response.data[key]) {
+              form.value[key] = new Date(response.data[key]);
+            } else {
+              form.value[key] = response.data[key];
+            }
+          }
+        });
+        readOnly.value = Boolean(props.data.readOnly);
+      } catch (err) {
+        console.error("Error loading data:", err);
+        toast.error("Não foi possível recuperar os dados do Aviso.");
+      } finally {
+        ready.value = true;
+      }
+    };
+    onMounted(async () => {
+      var _a;
+      if ((_a = props.data) == null ? void 0 : _a.id) {
+        acao.value = `/admin/avisos/${props.data.id}`;
+        await loadData();
+      } else {
+        ready.value = true;
+      }
+    });
+    return (_ctx, _push, _parent, _attrs) => {
+      if (ready.value) {
+        _push(`<div${ssrRenderAttrs(mergeProps({ class: "m-2" }, _attrs))}><form><div class="mb-4">`);
+        _push(ssrRenderComponent(_sfc_main$1, {
+          for: "nome",
+          value: "Nome",
+          class: "required"
+        }, null, _parent));
+        _push(ssrRenderComponent(_sfc_main$2, {
+          id: "nome",
+          class: "w-full",
+          modelValue: form.value.nome,
+          "onUpdate:modelValue": ($event) => form.value.nome = $event,
+          disabled: readOnly.value
+        }, null, _parent));
+        _push(ssrRenderComponent(_sfc_main$3, {
+          message: errors.value.nome
+        }, null, _parent));
+        _push(`</div><div class="mb-4">`);
+        _push(ssrRenderComponent(_sfc_main$1, {
+          for: "descricao",
+          value: "Descrição",
+          class: "required"
+        }, null, _parent));
+        _push(`<textarea id="descricao" class="w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"${ssrIncludeBooleanAttr(readOnly.value) ? " disabled" : ""} rows="5">${ssrInterpolate(form.value.descricao)}</textarea>`);
+        _push(ssrRenderComponent(_sfc_main$3, {
+          message: errors.value.descricao
+        }, null, _parent));
+        _push(`</div><div class="mb-4">`);
+        _push(ssrRenderComponent(_sfc_main$1, {
+          for: "data_expiração",
+          value: "Data de Expiração",
+          class: ["required", { "text-gray-400": readOnly.value }]
+        }, null, _parent));
+        _push(ssrRenderComponent(unref(VueDatePicker), {
+          id: "data_expiração",
+          modelValue: form.value.data_expiração,
+          "onUpdate:modelValue": ($event) => form.value.data_expiração = $event,
+          locale: "pt-BR",
+          disabled: readOnly.value,
+          format: "dd/MM/yyyy",
+          "enable-time-picker": false,
+          "auto-apply": "",
+          clearable: false,
+          placeholder: "Selecione uma data",
+          class: [{ "opacity-50": readOnly.value }, "w-full"]
+        }, null, _parent));
+        _push(ssrRenderComponent(_sfc_main$3, {
+          message: errors.value.data_expiração
+        }, null, _parent));
+        _push(`</div><div class="mb-4"><label class="flex items-center"><input type="checkbox" id="ativo" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500"${ssrIncludeBooleanAttr(Array.isArray(form.value.ativo) ? ssrLooseContain(form.value.ativo, null) : form.value.ativo) ? " checked" : ""}${ssrIncludeBooleanAttr(readOnly.value) ? " disabled" : ""}><span class="ml-2 text-sm text-gray-600">Ativo</span></label>`);
+        _push(ssrRenderComponent(_sfc_main$3, {
+          message: errors.value.ativo
+        }, null, _parent));
+        _push(`</div><div class="w-full pt-4 mt-4 border-t border-gray-200">`);
+        if (readOnly.value) {
+          _push(`<div class="flex justify-center"><button type="button" class="px-4 py-2 text-white bg-red-600 rounded-md hover:bg-red-700"><i class="mr-1 fa fa-close"></i> Sair </button></div>`);
+        } else {
+          _push(`<!---->`);
+        }
+        if (!readOnly.value) {
+          _push(`<div class="flex justify-center space-x-2"><button type="submit" class="px-4 py-2 text-white rounded-md bg-blue-500 hover:bg-blue-500-hover"${ssrIncludeBooleanAttr(processing.value) ? " disabled" : ""}>`);
+          if (!processing.value) {
+            _push(`<i class="mr-1 fa fa-check"></i>`);
+          } else {
+            _push(`<i class="mr-1 fa fa-spinner fa-spin"></i>`);
+          }
+          _push(` ${ssrInterpolate(processing.value ? "Salvando..." : "Salvar")}</button><button type="button" class="px-4 py-2 text-white bg-red-600 rounded-md hover:bg-red-700"><i class="mr-1 fa fa-close"></i> Cancelar </button></div>`);
+        } else {
+          _push(`<!---->`);
+        }
+        _push(`</div></form></div>`);
+      } else {
+        _push(`<!---->`);
+      }
+    };
+  }
+};
+const _sfc_setup = _sfc_main.setup;
+_sfc_main.setup = (props, ctx) => {
+  const ssrContext = useSSRContext();
+  (ssrContext.modules || (ssrContext.modules = /* @__PURE__ */ new Set())).add("resources/js/Pages/Admin/Avisos/AvisosForm.vue");
+  return _sfc_setup ? _sfc_setup(props, ctx) : void 0;
+};
+export {
+  _sfc_main as default
+};
---
+++ bootstrap/ssr/assets/CardapioIndex-2bZiMcZ6.js
@@ -0,0 +1,137 @@
+import { inject, ref, withCtx, createVNode, createTextVNode, useSSRContext } from "vue";
+import { ssrRenderComponent } from "vue/server-renderer";
+import { D as Datatable } from "./Datatable-DbtrSLIX.js";
+import { L as LayoutPrincipal } from "./LayoutPrincipal-BDM4AF6K.js";
+import _sfc_main$1 from "./PopupButton-DLi_hd91.js";
+import { useToast } from "vue-toastification";
+import "pinia";
+import "./_plugin-vue_export-helper-1tPrXgE0.js";
+import "vue3-daterange-picker";
+import "moment/moment.js";
+import "moment";
+import "@inertiajs/vue3";
+import "./Modal-D4SsQX3D.js";
+import "axios";
+const _sfc_main = {
+  __name: "CardapioIndex",
+  __ssrInlineRender: true,
+  setup(__props) {
+    useToast();
+    inject("events");
+    const source = ref("/admin/cardapio/list");
+    const columns = ref([
+      { name: "nome", title: "Nome", width: "20%", sort: "nome", nowrap: true },
+      { name: "descricao", title: "Descricao", width: "20%", sort: "descricao", nowrap: true },
+      { name: "valor_total", title: "Valor Total", width: "20%", sort: "valor_total", nowrap: true },
+      {
+        name: "id",
+        title: "Ações",
+        width: "10%",
+        nowrap: true,
+        contentClass: "text-center",
+        headerClass: "text-center",
+        template: "dropdown",
+        formatter: (val, row) => [
+          {
+            type: "anchor",
+            icon: "fa-tag",
+            href: `/admin/cardapio/${row.id}/ingredientes`,
+            text: "Ingredientes"
+          },
+          {
+            type: "modal",
+            icon: "fa-edit",
+            dataSize: "xl",
+            dataComponent: "CardapioForm",
+            dataTitle: "Editar  Cardapio",
+            dataJson: { id: row.id },
+            text: "Editar"
+          },
+          {
+            type: "delete",
+            icon: "fa-trash",
+            text: "Remover",
+            deleteUrl: `/admin/cardapio/${row.id}`,
+            dataTitle: "Confirmação de Remoção",
+            dataMessage: "Você deseja realmente excluir este registro?"
+          }
+        ]
+      }
+    ]);
+    return (_ctx, _push, _parent, _attrs) => {
+      _push(ssrRenderComponent(LayoutPrincipal, _attrs, {
+        default: withCtx((_, _push2, _parent2, _scopeId) => {
+          if (_push2) {
+            _push2(`<div class="page-content"${_scopeId}><div class="flex items-center justify-between mb-4 w-100"${_scopeId}><h2 class="text-2xl font-semibold text-primary"${_scopeId}></h2><div class="flex"${_scopeId}>`);
+            _push2(ssrRenderComponent(_sfc_main$1, {
+              id: "novo-cardapio",
+              title: "Novo  Cardapio",
+              size: "xl",
+              component: "CardapioForm",
+              variant: "secondary"
+            }, {
+              default: withCtx((_2, _push3, _parent3, _scopeId2) => {
+                if (_push3) {
+                  _push3(`<i class="mr-2 fa fa-plus"${_scopeId2}></i> Novo Cardapio `);
+                } else {
+                  return [
+                    createVNode("i", { class: "mr-2 fa fa-plus" }),
+                    createTextVNode(" Novo Cardapio ")
+                  ];
+                }
+              }),
+              _: 1
+            }, _parent2, _scopeId));
+            _push2(`</div></div><div${_scopeId}>`);
+            _push2(ssrRenderComponent(Datatable, {
+              id: "cardapio",
+              columns: columns.value,
+              source: source.value
+            }, null, _parent2, _scopeId));
+            _push2(`</div></div>`);
+          } else {
+            return [
+              createVNode("div", { class: "page-content" }, [
+                createVNode("div", { class: "flex items-center justify-between mb-4 w-100" }, [
+                  createVNode("h2", { class: "text-2xl font-semibold text-primary" }),
+                  createVNode("div", { class: "flex" }, [
+                    createVNode(_sfc_main$1, {
+                      id: "novo-cardapio",
+                      title: "Novo  Cardapio",
+                      size: "xl",
+                      component: "CardapioForm",
+                      variant: "secondary"
+                    }, {
+                      default: withCtx(() => [
+                        createVNode("i", { class: "mr-2 fa fa-plus" }),
+                        createTextVNode(" Novo Cardapio ")
+                      ]),
+                      _: 1
+                    })
+                  ])
+                ]),
+                createVNode("div", null, [
+                  createVNode(Datatable, {
+                    id: "cardapio",
+                    columns: columns.value,
+                    source: source.value
+                  }, null, 8, ["columns", "source"])
+                ])
+              ])
+            ];
+          }
+        }),
+        _: 1
+      }, _parent));
+    };
+  }
+};
+const _sfc_setup = _sfc_main.setup;
+_sfc_main.setup = (props, ctx) => {
+  const ssrContext = useSSRContext();
+  (ssrContext.modules || (ssrContext.modules = /* @__PURE__ */ new Set())).add("resources/js/Pages/Admin/Cardapio/CardapioIndex.vue");
+  return _sfc_setup ? _sfc_setup(props, ctx) : void 0;
+};
+export {
+  _sfc_main as default
+};
