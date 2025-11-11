a294b7cf2892bb57ba80f3c189af2a605fd3b9d8
dianodev <adrianomellocolombo@gmail.com>
2025-11-11 14:01:18 -0400

    Adição de 2 tabelas cardapio_evento

---
+++ database/migrations/2025_10_20_182051_cardapio_evento.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('cardapio_evento', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_evento')->constrained('evento')->onDelete('cascade');
+            $table->string('nome', 255);
+            $table->string('descricao')->nullable();
+            $table->decimal('valor_total', 15, 2);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('cardapio_evento');
+    }
+};
---
+++ database/migrations/2025_10_20_182051_ingredientes_cardapio_evento.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('ingredientes_cardapio_evento', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_cardapio_evento')->constrained('eventos')->onDelete('cascade');
+            $table->foreignId('id_pessoa')->constrained('pessoas')->onDelete('cascade');
+            $table->string('nome', 255);
+            $table->string('quantidade')->nullable();
+            $table->string('unidade_medida');
+            $table->decimal('valor_unitario', 15, 2);
+            $table->decimal('valor_total', 15, 2);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('cardapio_evento');
+    }
+};
